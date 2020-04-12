7483299059615dfe1f573e8483a5a90fa97e48d3
checkchanin <63404482+checkchanin@users.noreply.github.com>
2020-04-12 15:36:58 +0700

    Done Mission 1

---
+++ src/main/java/check/demo01/Demo01Application.java
@@ -0,0 +1,11 @@
+package check.demo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo01Application {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldController.class, args);
+	}
+}
